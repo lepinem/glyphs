b4076ab43c39683bbf0245acd03622e5b6032d26
Matthew LePine <lepinem2002@gmail.com>
2018-03-08 02:30:02 -0500

    refactor1

---
+++ services/passport.js
@@ -0,0 +1,20 @@
+// passport.js
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../config/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accressToken", accessToken);
+      console.log("refreshToken", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
