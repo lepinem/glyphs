42ec396c71ff7d0e600c6f442d129449ffbd48ef
Matthew LePine <lepinem2002@gmail.com>
2018-03-14 02:17:54 -0400

    refactor

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+// App.js
+
+import React from "react";
+
+const App = () => {
+  return <div>Sippy time!</div>;
+};
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -25,17 +25,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       console.log(profile);
-      User.findOne({ googleID: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleID: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleID: profile.id });
+
+      if (existingUser) {
+        done(null, existingUser);
+      }
+
+      const user = await new User({ googleID: profile.id }).save();
+      done(null, user);
     }
   )
 );
